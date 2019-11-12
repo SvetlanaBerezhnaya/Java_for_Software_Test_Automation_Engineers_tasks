ec8ff6510ee6741a28474b746bc28909c65080a1
Svetlana Berezhnaya <51880203+SvetlanaBerezhnaya@users.noreply.github.com>
2019-11-12 19:36:42 +0200

    All tasks of Homework № 7 on Java have completed.

---
+++ Homework7/ValidatorException.java
@@ -0,0 +1,12 @@
+package com.epam.task1;
+
+public class ValidatorException extends Exception {
+
+    public ValidatorException(String message) {
+        super(message);
+    }
+
+    public ValidatorException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
